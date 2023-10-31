edbdc06a89b992a71a1fa023893eef7bd3112dc0
Joshua Morris <joshua.morris@hey.com>
2023-10-31 16:51:55 -0500

    oops: no need to 'puts' Area.plot_ascii_array

--- spec/lib/area_spec.rb
+++ spec/lib/area_spec.rb
@@ -28,35 +28,35 @@ end
 describe Area, ".max" do
   it "returns 2 for [1,1,8]" do
     inputs = [1, 1, 8]
-    puts Area.plot_ascii_array(inputs)
+    Area.plot_ascii_array(inputs)
     analysis = Area.max(inputs)
     expect(analysis.max_area).to eq(2)
   end
 
   it "returns 2 for [1,2,8]" do
     inputs = [1, 2, 8]
-    puts Area.plot_ascii_array(inputs)
+    Area.plot_ascii_array(inputs)
     analysis = Area.max(inputs)
     expect(analysis.max_area).to eq(2)
   end
 
   it "returns 16 for [8, 8, 8]" do
     inputs = [8, 8, 8]
-    puts Area.plot_ascii_array(inputs)
+    Area.plot_ascii_array(inputs)
     analysis = Area.max(inputs)
     expect(analysis.max_area).to eq(16)
   end
 
   it "returns 24 for [8, 8, 8, 9]" do
     inputs = [8, 8, 8, 9]
-    puts Area.plot_ascii_array(inputs)
+    Area.plot_ascii_array(inputs)
     analysis = Area.max(inputs)
     expect(analysis.max_area).to eq(24)
   end
 
   it "returns 49 for [1,8,6,2,5,4,8,3,7]" do
     inputs = [1, 8, 6, 2, 5, 4, 8, 3, 7]
-    puts Area.plot_ascii_array(inputs)
+    Area.plot_ascii_array(inputs)
     analysis = Area.max(inputs)
     expect(analysis.max_area).to eq(49)
   end
